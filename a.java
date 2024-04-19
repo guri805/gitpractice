df152a4000ceb6dd81ca2aace90eab837a2b3b93
guri805 <gursangamsingh2@gmail.com>
2024-04-19 14:34:29 +0530

    First Ever Commit

---
+++ a.java
@@ -0,0 +1,7 @@
+package gitpractice;
+
+public class a {
+    public static void main(String[] args) {
+        System.out.println("hello brother");
+    }
+}
---
+++ b.java
@@ -0,0 +1,7 @@
+package gitpractice;
+
+public class b {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
